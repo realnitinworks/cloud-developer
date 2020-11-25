68970c35d7e56e28170b21efe820d1a68e293221
realnitinworks <realnitinworks@gmail.com>
2020-11-25 05:58:35 +0530

    Add code to upload image to s3 using lambda

--- course-04/myexperiments/10-udagram-app/src/lambda/http/createImage.ts
+++ course-04/myexperiments/10-udagram-app/src/lambda/http/createImage.ts
@@ -3,10 +3,17 @@ import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
 
+
 const docClient = new AWS.DynamoDB.DocumentClient()
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
 
 const groupsTable = process.env.GROUPS_TABLE
 const imagesTable = process.env.IMAGES_TABLE
+const bucketName = process.env.IMAGES_S3_BUCKET
+// const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -28,6 +35,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         }
     }
 
+    // Create an entry in Dynamo DB table
     const imageId = uuid.v4();
     const timestamp = (new Date()).toJSON();  // toISOString() instead
     const parsedBody = JSON.parse(event.body);
@@ -35,13 +43,17 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         groupId: groupId,
         imageId: imageId,
         timestamp: timestamp,
-        ...parsedBody
+        ...parsedBody,
+        imageUrl: `https://${bucketName}.s3.amazonaws.com/${imageId}` // User can use this URL to see the image
     }
 
     await docClient.put({
         TableName: imagesTable,
         Item: newImage
     }).promise()
+
+    // Get a signed url for putting an image into S3
+    const url = getUploadUrl(imageId);
     
     return {
         statusCode: 201,
@@ -49,7 +61,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
             'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            newItem: newImage
+            newItem: newImage,
+            uploadUrl: url  // User can use this url to put an image into S3
         })
     }
 }
@@ -64,4 +77,13 @@ async function groupExists(groupId: string) {
 
     console.log('Get Group: ', result);
     return !!result.Item
+}
+
+
+function getUploadUrl(imageId: string) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: imageId,
+        Expires: 300
+    })
 }
