6a5dca1a3309df4b89d05b266c6f4a4e5c056c1b
realnitinworks <realnitinworks@gmail.com>
2020-11-28 08:26:52 +0530

    Use AWS Secrets Manager to store secret for authentication

--- course-04/myexperiments/10-udagram-app/src/lambda/auth/auth0Authorizer.ts
+++ course-04/myexperiments/10-udagram-app/src/lambda/auth/auth0Authorizer.ts
@@ -1,15 +1,24 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler} from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
 
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const auth0Secret = process.env.AUTH_0_SECRET;
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+
+const client = new AWS.SecretsManager();
+
+
+// Cache secret if a Lambda instance is reused
+let cachedSecret: string;
 
 
 export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
     try {
-        const decodedToken: JwtToken = verifyToken(event.authorizationToken);
+        const decodedToken: JwtToken = await verifyToken(event.authorizationToken);
         console.log('User was authorized');
 
         return {
@@ -45,7 +54,7 @@ export const handler: CustomAuthorizerHandler = async (event: CustomAuthorizerEv
 }
 
 
-function verifyToken(authHeader: string): JwtToken {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
     if (!authHeader) {
         throw new Error('No authorization header');
     }
@@ -56,7 +65,22 @@ function verifyToken(authHeader: string): JwtToken {
 
     const token = authHeader.split(' ')[1];
 
-    return verify(token, auth0Secret) as JwtToken;
+    const secretObject: any = await getSecret();
+    const secret = secretObject[secretField];
+
+    return verify(token, secret) as JwtToken;
 
     // A request has been authorized
+}
+
+async function getSecret() {
+    if (cachedSecret) return cachedSecret;
+
+    const data = await client.getSecretValue({
+        SecretId: secretId
+    }).promise();
+
+    cachedSecret = data.SecretString;
+
+    return JSON.parse(cachedSecret);
 }
