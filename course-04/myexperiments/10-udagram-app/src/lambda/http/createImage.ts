a088fb0508bd7140def776d3536e029ca4b227c2
realnitinworks <realnitinworks@gmail.com>
2020-11-28 19:26:52 +0530

    Add middy cors middleware to a Lambda function

--- course-04/myexperiments/10-udagram-app/src/lambda/http/createImage.ts
+++ course-04/myexperiments/10-udagram-app/src/lambda/http/createImage.ts
@@ -1,7 +1,11 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import * as uuid from 'uuid'
+
+
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
 
 const docClient = new AWS.DynamoDB.DocumentClient()
@@ -16,7 +20,7 @@ const bucketName = process.env.IMAGES_S3_BUCKET
 // const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('Processing event: ', event)
 
     const groupId = event.pathParameters.groupId;
@@ -26,9 +30,6 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     if (!validGroup) {
         return {
             statusCode: 404,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
             body: JSON.stringify({
                 error: "Group does not exist"
             })
@@ -57,15 +58,13 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     
     return {
         statusCode: 201,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
         body: JSON.stringify({
             newItem: newImage,
             uploadUrl: url  // User can use this url to put an image into S3
         })
     }
-}
+});
+
 
 async function groupExists(groupId: string) {
     const result = await docClient.get({
@@ -87,3 +86,10 @@ function getUploadUrl(imageId: string) {
         Expires: 300
     })
 }
+
+
+handler.use(
+    cors({
+        credentials: true
+    })
+);
