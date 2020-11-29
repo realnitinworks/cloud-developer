00f29517b2e87ab82617d98eb8202cdce5c2e6d8
realnitinworks <realnitinworks@gmail.com>
2020-11-29 12:20:01 +0530

    Refactor app into businessLogic and dataLayer

--- course-04/myexperiments/10-udagram-app/src/lambda/http/createGroup.ts
+++ course-04/myexperiments/10-udagram-app/src/lambda/http/createGroup.ts
@@ -1,32 +1,18 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
-import * as uuid from 'uuid'
-import { getUserId } from '../../auth/utils'
 
-const docClient = new AWS.DynamoDB.DocumentClient()
 
-const groupsTable = process.env.GROUPS_TABLE
+import { CreateGroupRequest } from '../../requests/CreateGroupRequest'
+import { createGroup } from '../../businessLogic/groups'
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log('Processing event: ', event)
-    const itemId = uuid.v4()
 
-    const parsedBody = JSON.parse(event.body)
-    const jwtToken = getToken(event); // encoded JWT Token in header
-    const userId = getUserId(jwtToken); // .sub from decoded JWT Token
-
-    const newItem = {
-        id: itemId,
-        userId,
-        ...parsedBody
-    }
-
-    await docClient.put({
-        TableName: groupsTable,
-        Item: newItem
-    }).promise()
+    const newGroup: CreateGroupRequest = JSON.parse(event.body)
+    const jwtToken: string = getToken(event); // encoded JWT Token in header
+    
+    const newItem = await createGroup(newGroup, jwtToken);
 
     return {
         statusCode: 201,
