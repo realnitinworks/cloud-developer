c60130fb9f8bc1c09cc1a1f38d1824a082e64225
realnitinworks <realnitinworks@gmail.com>
2020-11-30 12:17:27 +0530

    Use express in Lambda function

--- course-04/myexperiments/10-udagram-app/src/lambda/http/getGroups.ts
+++ course-04/myexperiments/10-udagram-app/src/lambda/http/getGroups.ts
@@ -1,21 +1,29 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 
+
+import * as express from 'express'
+import * as awsServerlessExpress from 'aws-serverless-express'
+
 import { getAllGroups } from "../../businessLogic/groups"
 
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    console.log('Processing event: ', event)
+const app = express();
+// Create Express server
+const server = awsServerlessExpress.createServer(app)
 
+
+app.get('/groups', async (_req, res) => {
+    // TODO: get all groups as before
     const groups = await getAllGroups();
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-            items: groups
-        })
-    }
+    // Return a list of groups
+    res.json({
+        items: groups
+    });
+})
+
+// Pass API Gateway events to the Express server
+exports.handler = (event, context) => { 
+    awsServerlessExpress.proxy(server, event, context)
 }
